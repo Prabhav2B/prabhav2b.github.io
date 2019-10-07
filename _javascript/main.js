2b8f4730c20af3e9516b1015be25a2fccccc9f92
Prabhav_b <prabhav2b@gmail.com>
2019-10-08 01:23:07 +0530

    proto

---
+++ _javascript/main.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Hello Bulma!');
+});
+import bulmaCalendar from '/node_modules/bulma-extensions/bulma-carousel/dist/js/bulma-carousel.min.js';
