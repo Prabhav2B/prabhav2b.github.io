1f9575a29e9fa309edaab75e9f7ad6f77543cced
Prabhav_b <prabhav2b@gmail.com>
2020-01-16 14:44:49 +0530

    Renovated
    Yea the old one sucked

--- _javascript/main.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
-});
-import bulmaCalendar from '/node_modules/bulma-extensions/bulma-carousel/dist/js/bulma-carousel.min.js';
